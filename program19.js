edfd4adbaac61523673bd8a90410297e0996f2f5
Suprio Adhikary <suprioadhikary802@gmail.com>
2026-09-03 22:17:50 +0530

    punctuator issue of code 19 fixed

--- program19.js
+++ program19.js
@@ -1,7 +1,15 @@
-let tech=["HTML","CSS","JavaScript"];
-let frontech=tech.reduce(
-  (accumulator,currentvalue)=>{
-    accumulator+=currentvalue+" , ";
-    return accumulator;
-  },"");
-  console.log(frontech);
+let tech = ["HTML", "CSS", "JavaScript"];
+let frontech = tech.reduce(
+  (accumulator, currentvalue) => {
+    if (accumulator === "") {
+      accumulator += currentvalue;
+      return accumulator;
+    }
+
+    else {
+      accumulator += ", " + currentvalue;
+      return accumulator;
+    }
+
+  }, "");
+console.log(`"${frontech}"`);
